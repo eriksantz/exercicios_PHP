9d4c446919c2974c98c37d29a299051ee7fe5c2f
Erik Santos <erik.pereira@aluno.fmpsc.edu.br>
2025-04-14 10:16:16 -0300

    Update exercicio10.php

--- aula88/exercicio10.php
+++ aula88/exercicio10.php
@@ -28,5 +28,13 @@
         }
     }
     ?>
+      <div class="botoes-navegacao">
+    <button onclick="history.back()">
+        🔙 Voltar
+    </button>
+    <button onclick="location.href='index.html'">
+        🏠 Home
+    </button>
+</div>
 </body>
 </html>
